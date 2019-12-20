bf3b2085a28c4d250768a9c56bb38359f5903c4f
vartika <vartika.ongraph@gmail.com>
2019-12-20 13:41:00 +0530

    testing

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
- 
 
 const Landing = ({ isAuthenticated }) => {
     if(isAuthenticated){
